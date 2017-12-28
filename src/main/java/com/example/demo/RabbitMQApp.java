07bfdb7815ba8c7a3ba2471eccebbff294dbcbb0
ninuxGithub <1732768613@qq.com>
2017-12-28 10:03:25 +0800

    mm

--- src/main/java/com/example/demo/RabbitMQApp.java
+++ src/main/java/com/example/demo/RabbitMQApp.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import java.net.InetSocketAddress;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
